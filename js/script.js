8e04465d37f7f018a0a11cdd793a2c23e5d8cfe7
Abdullah Zameek <arz268@nyu.edu>
2019-05-10 18:43:08 +0800

    added stuff

--- js/script.js
+++ js/script.js
@@ -1,6 +1,5 @@
 let model, img, ouputCanvas, count = 0;
 
-main();
 
 async function main() {
     tf.disableDeprecationWarnings();
@@ -9,8 +8,8 @@ async function main() {
     const WEIGHTS_URL = './models/weights_manifest.json';
     model = await tf.loadFrozenModel(MODEL_URL, WEIGHTS_URL);
 
-    img = document.getElementById('img');
-    outputCanvas = document.getElementById('outputCanvas');
+    img = document.getElementById("inter");
+    outputCanvas = document.getElementById("outputcanvas");
 
     predict();
 }
@@ -28,58 +27,11 @@ function predict() {
     tf.toPixels(postprocessed_tensor, outputCanvas);
 }
 
-function loadInput() {
-    let str;
-    count++;
-    if (count < 10) {
-        str = "0000" + count;
-    } else if (count < 100) { 
-        str = "000" + count; 
-    }   
-    if(count >= 100) count = 0;
-    
-    img.src = "img/" + str + ".jpg";
+function keyTyped(){
+    if(key === "t"){
+        console.log("started GAN....");
+        main();
+        console.log("GAN Done");
+    }
 }
 
-
-
-// let model;
-// let inputCanvas,outputCanvas; 
-
-// function preload(){
-//     tf.disableDeprecationWarnings();
-//     const MODEL_URL = './models/tensorflowjs_model.pb';
-//     const WEIGHTS_URL = './models/weights_manifest.json';
-//     model = tf.loadFrozenModel(MODEL_URL, WEIGHTS_URL);
-//     console.log(model);
-//     ouputCanvas = document.getElementById('outputCanvas');
-//     inputCanvas = document.getElementById('inputCanvas');
-// }
-
-// function setup(){
-
-// }
-
-// function draw(){
-
-// }
-
-// function keyPressed(){
-//     const img = loadImage('img/00001.jpg');
-//     inputCanvas.src = img;
-//     predict(inputCanvas,outputCanvas);
-// }
-
-
-// function predict(img,ouputCanvas){
-//     // int32, [0, 255], [256, 256, 3]
-//     let input_tensor = tf.fromPixels(img); 
-//     // preprocess: int32, [0, 255], [256, 256, 3]    =>    float, [-1, 1], [1, 256, 256, 3]
-//     let preprocessed_tensor = input_tensor.toFloat().div(tf.scalar(127.5)).sub(tf.scalar(1.0)).expandDims();
-//     // predict: [-1, 1] => [-1, 1]
-//     let output_tensor = model.predict(preprocessed_tensor);
-//     // postprocess: float, [-1, 1], [1, 256, 256, 3]   =>   float, [0, 1], [256, 256, 3]
-//     let postprocessed_tensor = output_tensor.mul(tf.scalar(0.5)).add(tf.scalar(0.5)).squeeze();
-//     // output image to canvas
-//     tf.toPixels(postprocessed_tensor, outputCanvas);
-// }
--- js/sketch.js
+++ js/sketch.js
@@ -6,10 +6,38 @@ function setup()
     canvas.parent('origin');
     video = createCapture(VIDEO);
     video.hide() // hides the html element
+    //saveThis = createGraphics(256, 256);
+
 }
 
 function draw()
 {   
-    fill(0);
-    rect(0,0,512,512);
+    background(0); 
+    image(video, 128, 128, 256, 256);
+    
+}
+
+var base64image = "";
+
+function keyTyped(){
+    if (key === ' '){
+        console.log("tried to save image");
+        var c = get(128, 128, 256, 256);
+        c.loadPixels();
+        console.log(c);
+        base64image = c.canvas.toDataURL();
+        // console.log(base64image);
+        console.log(base64image.length);
+    }
+    if (key == 's'){
+        placeImage(base64image);
+    }
+}
+
+function placeImage(base64image){
+    console.log("change image  now");
+    var img = document.getElementById("inter");
+    img.src = base64image;
+    console.log(img.src);
+    
 }
